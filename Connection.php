2dffd88255959c09b73a56c8b864a0af1c43c63a
Alex <52381045+AlexandroLuis@users.noreply.github.com>
2021-11-07 18:39:50 -0300

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -2,18 +2,6 @@
 	Desenvolvido por https://github.com/AlexandroLuis
 	Versão 1.0 - 2020
 -->
-/*
-<?PHP 
-	$hostname='rds-mysql-uriexamples.cqrhkcdlpxn6.sa-east-1.rds.amazonaws.com';	//Nome do Servidor
-	$username='masterUsername';		//Nome do Usuario do servidor
-	$pass='G37SYckTZhFaxrh';				//Senha do servidor
-	$bd='UriExamples';		//Nome do banco de dados
-	
-	//Conecta ao banco de dados
-	$db=Mysqli_Connect($hostname, $username, $pass);	 
-	Mysqli_Select_db($db, $bd);	
-?>
-*/
 <?PHP 
 	$hostname='localhost';	//Nome do Servidor
 	$username='root';		//Nome do Usuario do servidor
